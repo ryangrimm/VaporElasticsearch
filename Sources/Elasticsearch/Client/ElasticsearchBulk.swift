17a2873803b3c288f4539316eaf08fa9120f37c2
Ryan Grimm <ryan@ryangrimm.com>
2018-07-14 14:31:31 -0700

    Added some docs.

--- Sources/Elasticsearch/Client/ElasticsearchBulk.swift
+++ Sources/Elasticsearch/Client/ElasticsearchBulk.swift
@@ -3,18 +3,32 @@ import Foundation
 import HTTP
 
 extension ElasticsearchClient {
+    /// Creates a new bulk operation.
+    ///
+    /// [Here's more information on bulk operations](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html)
+    ///
+    /// - Parameter capacity: This is the estimated number of bytes for the body of the request. Defaults to 10MB.
+    /// - Returns: Returns an `ElasticsearchBulk` class for adding operations to.
     public func bulkOperation(estimatedBodySize capacity: Int = 10 * 1024 * 1024)  -> ElasticsearchBulk {
         return ElasticsearchBulk(client: self, estimatedBodySize: capacity)
     }
 }
 
+
+/// Configures the header used for each bulk operation
 public struct BulkHeader: Encodable {
-    var index: String?
-    var id: String?
-    var type: String?
-    var routing: String?
-    var version: Int?
-    var retryOnConflict: Int?
+    /// The index to use
+    public var index: String?
+    /// The document id
+    public var id: String?
+    /// The ElasticSearch type (defaults to "_doc")
+    public var type: String?
+    /// Routing information
+    public var routing: String?
+    /// Document version
+    public var version: Int?
+    /// Number of retry attempts (only used in `update` commands)
+    public var retryOnConflict: Int?
     
     enum CodingKeys: String, CodingKey {
         case index = "_index"
@@ -26,18 +40,40 @@ public struct BulkHeader: Encodable {
     }
 }
 
+/// Bulk operation management.
+///
+/// [Here's more information on bulk operations](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html)
 public class ElasticsearchBulk {
     var requestBody:Data
     let encoder = JSONEncoder()
     let client: ElasticsearchClient
     
+    /// Allows default values to be applied to all bulk operations.
+    ///
+    /// This is useful in cases where all (or the majority) of the opeations you perform should be performed on the same index, type, etc.
+    /// If values are provided in the operation calls (`index`, `create`, `update`, `delete`) those values override the values provided here.
     public var defaultHeader: BulkHeader = BulkHeader(index: nil, id: nil, type: "_doc", routing: nil, version: nil, retryOnConflict: nil)
     
+    /// Creates a new bulk operation object.
+    ///
+    /// - Parameters:
+    ///   - client: The client to issue the bulk operation on
+    ///   - capacity: The estimated size of the total body in bytes
     init(client: ElasticsearchClient, estimatedBodySize capacity: Int) {
         self.client = client
         requestBody = Data(capacity: capacity)
     }
     
+    /// Add a document to be indexed to the bulk operation
+    ///
+    /// - Parameters:
+    ///   - doc: The document to be indexed
+    ///   - index: The index to put the document into
+    ///   - id: The id to use for the document
+    ///   - type: The document type
+    ///   - routing: Routing information
+    ///   - version: Version information
+    /// - Throws: Can throw errors if there are issues encoding the doc.
     public func index<T: Codable>(
         doc :T,
         index: String? = nil,
@@ -56,6 +92,16 @@ public class ElasticsearchBulk {
         requestBody.append(10)
     }
     
+    /// Add a document to be created to the bulk operation
+    ///
+    /// - Parameters:
+    ///   - doc: The document to be created
+    ///   - index: The index to put the document into
+    ///   - id: The id to use for the document
+    ///   - type: The document type
+    ///   - routing: Routing information
+    ///   - version: Version information
+    /// - Throws: Can throw errors if there are issues encoding the doc.
     public func create<T: Codable>(
         doc :T,
         index: String? = nil,
@@ -74,6 +120,16 @@ public class ElasticsearchBulk {
         requestBody.append(10)
     }
     
+    /// Add a document to be updated to the bulk operation
+    ///
+    /// - Parameters:
+    ///   - doc: The document to be updated
+    ///   - index: The index to put the document into
+    ///   - id: The id to use for the document
+    ///   - type: The document type
+    ///   - routing: Routing information
+    ///   - version: Version information
+    /// - Throws: Can throw errors if there are issues encoding the doc.
     public func update<T: Codable>(
         doc :T,
         index: String? = nil,
@@ -93,6 +149,15 @@ public class ElasticsearchBulk {
         requestBody.append(10)
     }
     
+    /// Add a document to be deleted by the bulk operation
+    ///
+    /// - Parameters:
+    ///   - id: The id of the document to be deleted
+    ///   - index: The index where the document is located at
+    ///   - type: The document type
+    ///   - routing: Routing information
+    ///   - version: Version information
+    /// - Throws: Can throw errors if there are issues encoding the doc.
     public func delete(
         id: String? = nil,
         index: String? = nil,
@@ -106,6 +171,10 @@ public class ElasticsearchBulk {
         requestBody.append(10)
     }
     
+    /// Executes the bulk operation
+    ///
+    /// - Returns: Returns the response from the bulk operation
+    /// - Throws: Bad stuff
     public func send() throws -> Future<BulkResponse> {
         // Add on a newline at the end to signal the end of the commands
         requestBody.append(10)
